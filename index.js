3963096a9c585f370556bfc5d1ab904a264452ab
Jerome Burkard <jerome.burkard@gadz.org>
2018-02-06 15:10:44 +0100

    Implementing mongoDb

--- index.js
+++ index.js
@@ -5,79 +5,153 @@ const morgan = require("morgan");
 const multer = require("multer");
 const upload = multer({ dest: "public/uploads/" });
 const _ = require("lodash");
+const chalk = require("chalk");
+
+const mongoose = require("mongoose");
+
+mongoose.connect("mongodb://localhost:27017/leboncoin");
+
+const superAdSchema = mongoose.Schema({
+	title: {
+		type: String,
+		required: true
+	},
+	description: String,
+	city: String,
+	price: Number,
+	photo: String
+});
+
+const SuperAd = mongoose.model("SuperAd", superAdSchema);
 
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(
-	morgan(":method :url :status :response-time ms - :res[content-length]")
-);
+app.use(morgan("dev"));
+// app.use(
+// 	morgan(function(tokens, req, res) {
+// 		var status = tokens.status(req, res);
+// 		var statusColor =
+// 			status >= 500
+// 				? "red"
+// 				: status >= 400 ? "yellow" : status >= 300 ? "cyan" : "green";
 
-const ads = [];
+// 		return (
+// 			chalk.reset(
+// 				padRight(tokens.method(req, res) + " " + tokens.url(req, res), 30)
+// 			) +
+// 			" " +
+// 			chalk[statusColor](status) +
+// 			" " +
+// 			chalk.reset(padLeft(tokens["response-time"](req, res) + " ms", 8)) +
+// 			" " +
+// 			chalk.reset("-") +
+// 			" " +
+// 			chalk.reset(tokens.res(req, res, "content-length") || "-")
+// 		);
+// 	})
+// );
 
-function getNewId() {
-	var i = 0;
-	ads.forEach(ad => {
-		id = parseInt(ad.id);
-		if (i < id) {
-			i = id;
-		}
-	});
-	return (i + 1).toString();
-}
+// function padLeft(str, len) {
+// 	return len > str.length
+// 		? new Array(len - str.length + 1).join(" ") + str
+// 		: str;
+// }
+// function padRight(str, len) {
+// 	return len > str.length
+// 		? str + new Array(len - str.length + 1).join(" ")
+// 		: str;
+// }
 
-const seed = {
-	id: 1,
+const seed = new SuperAd({
 	title: "Tom",
 	description:
 		"Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe porro expedita illo possimus repellendus itaque dolorem illum, veritatis omnis reprehenderit ratione a sit, nam earum labore consectetur minus nulla doloremque.",
 	city: "Paris",
 	price: 25,
 	photo: "73285e1f2bb198b4afc9c49b1c425a5f"
-};
+});
 
-ads.push(seed);
-console.log(ads);
+// seed.save(function(err, obj) {
+// 	if (err) {
+// 		console.log("something went wrong when saving");
+// 	} else {
+// 		console.log("The ad was saved :" + obj);
+// 	}
+// });
 
+// INDEX
 app.get("/", function(req, res) {
-	console.log("ads :", ads);
-	res.render("home.ejs", {
-		ads
+	SuperAd.find({}, function(err, ads) {
+		if (!err) {
+			console.log("ads :", ads);
+			res.render("home.ejs", {
+				ads: ads
+			});
+		}
 	});
 });
 
+// redirection to home : index
 app.get("/offres/", function(req, res) {
 	res.redirect("/");
 });
 
+// SHOW
 app.get("/annonce/:id", function(req, res) {
-	console.log("type of req.params.id :", typeof req.params.id);
-	const index = _.findIndex(ads, function(ad) {
-		console.log("type of ad.id :", typeof ad.id);
-		return ad.id.toString() === req.params.id;
-	});
-	console.log("index :", index);
-	res.render("show.ejs", {
-		id: ads[index].id,
-		title: ads[index].title,
-		description: ads[index].description,
-		city: ads[index].city,
-		price: ads[index].price,
-		photo: ads[index].photo
+	SuperAd.findOne({ _id: req.params.id }, function(err, ad) {
+		if (!err) {
+			console.log("ad :", ad);
+			res.render("show.ejs", {
+				id: ad._id,
+				title: ad.title,
+				description: ad.description,
+				city: ad.city,
+				price: ad.price,
+				photo: ad.photo
+			});
+		} else {
+			console.log("An error occured: " + err);
+		}
 	});
 });
 
+// EDIT:
+app.get("/modification/:id", function(req, res) {
+	SuperAd.findOne({ _id: req.params.id }, function(err, ad) {
+		if (!err) {
+			console.log("ad in modif :", ad);
+			res.render("edit.ejs", {
+				title: ad.title,
+				description: ad.description,
+				city: ad.city,
+				price: ad.price,
+				photo: ad.photo
+			});
+		} else {
+			console.log("An error occured: " + err);
+		}
+	});
+});
+
+// NEW:
 app.get("/deposer/", function(req, res) {
-	res.render("new.ejs", {
-		id: getNewId()
-	});
+	res.render("new.ejs");
 });
 
+// CREATE:
 app.post("/add_ad/", upload.single("photo"), function(req, res) {
-	var obj = req.body;
-	obj[photo] = req.file.filename;
-	ads.push(req.body);
-	res.redirect("/annonce/" + req.body.id);
-	console.log("ads :", ads);
+	var obj = new SuperAd(req.body);
+	obj.photo = req.file.filename;
+
+	obj.save(function(err, obj) {
+		if (err) {
+			console.log("something went wrong when saving");
+			res.send("something went wrong when saving");
+		} else {
+			console.log("The ad was saved :" + obj);
+			res.redirect("/annonce/" + req.body.id);
+		}
+	});
 });
 
 app.listen(3000, function() {
